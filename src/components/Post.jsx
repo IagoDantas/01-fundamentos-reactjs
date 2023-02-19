0d5e78d46df20e82a10e1949ac71889584db5146
Iago Dantas <iagodantas.8@gmail.com>
2023-02-18 22:27:14 -0300

    post styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,9 +5,34 @@ export function Post(){
     return(
         <article className={styles.post}>
             <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/IagoDantas.png"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Iago Dantas</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
 
+
+                <time title="11 de Maio as 08:13" dateTime='2022-05-11 08:13:00'>
+                    Publicado há 1h
+                </time>
+                
             </header>
+            <div className={styles.content}>
+                <p>Falaa galera 👋🏽👋🏽</p>
 
+
+                <p>Acabei de subir mais um projeto no meu portifa, É um projeto que fiz no NLW Return, evento da Rocketseat</p>
+
+                <p>👉🏽{' '}<a href="#">iago.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto </a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>{' '}
+                </p>
+            </div>
         </article>
     );
 }
